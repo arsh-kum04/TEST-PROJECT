966e8b47b15e6b0e51a767f469996e79ef305e97
KNOW YOUR CODE <XYZ@gmail.com>
2024-04-05 23:22:27 +0530

    Add documentation for ExampleUnitTest.kt

---
+++ app\src\test\java\com\example\notesapp_internshala\ExampleUnitTest.kt
@@ -0,0 +1,20 @@
+**Folder Name:** com.example.notesapp_internshala
+
+**File Name:** ExampleUnitTest.kt
+
+```
+// Line by line documented Code
+package com.example.notesapp_internshala
+
+import org.junit.Test
+import org.junit.Assert.*
+// This class is used for unit testing.
+class ExampleUnitTest {
+    // This function is annotated with @Test, indicating it should be run as a test case.
+    @Test
+    fun addition_isCorrect() {
+        // This line asserts that the result of 2 + 2 is equal to 4.
+        assertEquals(4, 2 + 2)
+    }
+}
+```
