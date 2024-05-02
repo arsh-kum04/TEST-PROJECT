726f5ec69e5bcc64739e8ebc926edc5b1a7fe44a
Arsh Kumar <arshkum18@gmail.com>
2024-05-02 13:03:09 +0530

    Add documentation for app\src\androidTest\java\com\example\notesapp_internshala\ExampleInstrumentedTest.kt

--- app\src\androidTest\java\com\example\notesapp_internshala\ExampleInstrumentedTest.kt
+++ app\src\androidTest\java\com\example\notesapp_internshala\ExampleInstrumentedTest.kt
@@ -2,7 +2,6 @@
 
 **File Name: ExampleInstrumentedTest.java**
 
-```
 // Import the necessary libraries
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -17,13 +16,15 @@ import org.junit.Assert.*;
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class) // Run the test using JUnit4
+// Run the test using JUnit4
+@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
+    // Test the application context
     fun useAppContext() {
-        // Context of the app under test.
+        // Get the context of the app under test
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.notesapp_internshala", appContext.packageName) // Assert that the package name of the app under test is correct
+        // Assert that the package name of the app under test is correct
+        assertEquals("com.example.notesapp_internshala", appContext.packageName)
     }
 }
-```
