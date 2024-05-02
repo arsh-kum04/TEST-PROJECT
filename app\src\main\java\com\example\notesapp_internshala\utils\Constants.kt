be260d45d6493bcc9c9b5a3098336fbe2527d2fe
Arsh Kumar <arshkum18@gmail.com>
2024-05-02 13:05:48 +0530

    Add documentation for app\src\main\java\com\example\notesapp_internshala\utils\Constants.kt

--- app\src\main\java\com\example\notesapp_internshala\utils\Constants.kt
+++ app\src\main\java\com\example\notesapp_internshala\utils\Constants.kt
@@ -16,11 +16,20 @@ public class Constants {
     // Define a constant variable NOTES to store the name of the collection that will hold the notes
     public static final String NOTES = "notes";
 
+    // Line by line documentation:
+    // This is a constant variable that stores the name of the collection that will hold the notes.
+
     // Define a constant variable CURRENT_USER_ID to store the ID of the current user
     public static final String CURRENT_USER_ID = "currentUserId";
 
+    // Line by line documentation:
+    // This is a constant variable that stores the ID of the current user.
+
     // Define a constant variable TIMESTAMP to store the timestamp of the last update
     public static final String TIMESTAMP = "timestamp";
+
+    // Line by line documentation:
+    // This is a constant variable that stores the timestamp of the last update.
 
     // Define a method to get the application context
     public static Context getContext() {
@@ -28,10 +37,16 @@ public class Constants {
         return com.example.notesapp_internshala.NoteApplication.getContext();
     }
 
+    // Line by line documentation:
+    // This method returns the application context.
+
     // Define a method to log a message to the console
     public static void log(String message) {
         // Log the message to the console
         Log.d("Constants", message);
     }
+
+    // Line by line documentation:
+    // This method logs a message to the console.
 }
 ```
