051223f1c1b8cc7af90c4d6ca10952a003def3a4
Arsh Kumar <arshkum18@gmail.com>
2024-05-02 13:03:01 +0530

    Add documentation for ExampleUnitTest.kt

--- app/src/test/java/com/example/notesapp_internshala/ExampleUnitTest.kt
+++ app/src/test/java/com/example/notesapp_internshala/ExampleUnitTest.kt
@@ -1,17 +1,26 @@
-package com.example.notesapp_internshala
+**Folder Name:** com.example.notesapp_internshala
 
-import org.junit.Test
+**File Name:** ExampleUnitTest.java
 
-import org.junit.Assert.*
+```java
+// This class contains unit tests for the Notes App developed using the Internshala platform.
+
+import org.junit.Test;
+import org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * This class contains unit tests for the Notes App developed using the Internshala platform.
  *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-class ExampleUnitTest {
+public class ExampleUnitTest {
+    // This method tests the addition operation and verifies if the result is correct.
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    public void addition_isCorrect() {
+        // This line asserts that the addition of 2 and 2 equals 4. If the assertion fails, the test will fail.
+
+        assertEquals(4, 2 + 2);
     }
 }
+```
