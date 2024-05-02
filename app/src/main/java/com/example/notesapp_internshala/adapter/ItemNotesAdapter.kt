9241768d7064ec0d4daf0882adc029109c100b96
Arsh Kumar <arshkum18@gmail.com>
2024-05-02 13:00:48 +0530

    Add documentation for ItemNotesAdapter.kt

--- app/src/main/java/com/example/notesapp_internshala/adapter/ItemNotesAdapter.kt
+++ app/src/main/java/com/example/notesapp_internshala/adapter/ItemNotesAdapter.kt
@@ -1,3 +1,9 @@
+**Folder Name: adapter**
+
+**File Name: ItemNotesAdapter.kt**
+
+```kotlin
+// Import required Android libraries and data model
 package com.example.notesapp_internshala.adapter
 
 import android.view.LayoutInflater
@@ -6,17 +12,25 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.notesapp_internshala.databinding.ItemsNotesBinding
 import com.example.notesapp_internshala.models.Note
 
+// Adapter class to bind data to RecyclerView
 class ItemNotesAdapter(private val list: ArrayList<Note>):
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private var listener: OnNoteItemClickListener? = null
 
+    // Interface to handle item clicks
+    interface OnNoteItemClickListener {
+        fun onEditClick(position: Int,  note: Note)
+        fun onDeleteClick(position: Int, noteId: String )
+    }
 
+    // Inner class to hold the view references
     class MyViewHolder(binding: ItemsNotesBinding): RecyclerView.ViewHolder(binding.root) {
         val tvNoteTitle = binding.notesTitle
         val tvNoteDescription = binding.notesBody
         val ibEditNote = binding.editNotes
         val ibDeleteNote = binding.ibDeleteList
     }
+
+    // Create the view holder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return MyViewHolder(ItemsNotesBinding.inflate(
             LayoutInflater.from(parent.context),
@@ -24,38 +38,45 @@ class ItemNotesAdapter(private val list: ArrayList<Note>):
             false))
     }
 
+    // Return the size of the dataset
     override fun getItemCount(): Int {
         return list.size
     }
 
+    // Binds data to the view holder
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        // Get the current note
         val model = list[position]
+
+        // If the holder is an instance of MyViewHolder
         if(holder is MyViewHolder) {
+            // Set the title and description texts
             holder.tvNoteTitle.text = model.title
             holder.tvNoteDescription.text = model.description
 
+            // Set up the delete button click listener
             holder.ibDeleteNote.setOnClickListener {
+                // If the listener is set
                 if(listener != null) {
+                    // Call the onDeleteClick method of the listener
                     listener!!.onDeleteClick(position, model.documentId)
                 }
             }
 
+            // Set up the edit button click listener
             holder.ibEditNote.setOnClickListener {
+                // If the listener is set
                 if(listener != null) {
+                    // Call the onEditClick method of the listener
                     listener!!.onEditClick(position,  list[position])
                 }
             }
         }
     }
 
+    // Sets the click listener
     fun setOnClickListener(listener: OnNoteItemClickListener) {
         this.listener = listener
     }
-
-    interface OnNoteItemClickListener {
-        fun onEditClick(position: Int,  note: Note)
-        fun onDeleteClick(position: Int, noteId: String )
-    }
-
-
 }
+```
