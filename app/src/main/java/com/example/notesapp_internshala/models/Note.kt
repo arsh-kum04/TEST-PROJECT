e98d332e4cf817192c390fcbed98212a8ca07003
Arsh Kumar <arshkum18@gmail.com>
2024-05-03 09:32:30 +0530

    Add documentation for Note.kt

--- app/src/main/java/com/example/notesapp_internshala/models/Note.kt
+++ app/src/main/java/com/example/notesapp_internshala/models/Note.kt
@@ -1,55 +1,14 @@
-**Folder Name:** com.example.notesapp_internshala.models
-**File Name:** Note.java
+**Folder Name:** com.example.notesappinternshala.utils
+**File Name:** MyApplication.java
 
 **Code:**
 ```java
-// Data class that represents an individual note
-data class Note(
-    // The ID of the current user as a string
-    val currentUserId: String = "",
-    // The title of the note as a string
-    val title: String = "",
-    // The description of the note as a string
-    val description: String = "",
-    // The document ID of the note in the database as a string
-    var documentId: String = "",
-    // The timestamp of the note's creation as a long integer
-    val timestamp: Long = System.currentTimeMillis()
-): Parcelable {
+import android.app.Application;
 
-    // Constructor that initializes the Note object from a Parcel object
-    constructor(parcel: Parcel) : this(
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!
-    ) {
-    }
+public class MyApplication extends Application {
 
-    // Describe the contents of the Note object for parceling
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    // Write the Note object to a Parcel object
-    override fun writeToParcel(parcel: Parcel, p1: Int) {
-        parcel.writeString(title)
-        parcel.writeString(description)
-        parcel.writeString(documentId)
-    }
-
-    // Companion object that creates a Parcelable.Creator instance for the Note class
-    companion object CREATOR : Parcelable.Creator<Note> {
-
-        // Create a new Note object from a Parcel object
-        override fun createFromParcel(parcel: Parcel): Note {
-            return Note(parcel)
-        }
-
-        // Create a new Note object array of the specified size
-        override fun newArray(size: Int): Array<Note?> {
-            return arrayOfNulls(size)
-        }
-    }
+    // Initialize the application-wide singleton instance of the FirebaseFirestore instance
+    public static FirebaseFirestore db = FirebaseFirestore.getInstance();
 }
 ```
 
@@ -57,79 +16,22 @@ data class Note(
 
 **Line 1**:
 ```java
-data class Note( // Data class that represents an individual note
+import android.app.Application;
 ```
-- This line declares a data class named `Note`.
-- Data classes are used to represent structured data and provide automatic generation of getters, setters, and other methods.
-
-**Line 2**:
-```java
-    val currentUserId: String = "", // The ID of the current user as a string
-```
-- This line declares a property named `currentUserId` of type `String`.
-- It is initialized to an empty string by default.
-- This property represents the ID of the user who created the note.
+- This line imports the `Application` class from the Android SDK.
+- This class represents the foundation of any Android application and provides access to application-wide resources and functionality.
 
 **Line 3**:
 ```java
-    val title: String = "", // The title of the note as a string
+public class MyApplication extends Application {
 ```
-- This line declares a property named `title` of type `String`.
-- It is initialized to an empty string by default.
-- This property represents the title of the note.
-
-**Line 4**:
-```java
-    val description: String = "", // The description of the note as a string
-```
-- This line declares a property named `description` of type `String`.
-- It is initialized to an empty string by default.
-- This property represents the description of the note.
+- This line declares a public class named `MyApplication` that extends the `Application` class.
+- This class acts as the entry point for the Android application and manages its lifecycle.
 
 **Line 5**:
 ```java
-    var documentId: String = "", // The document ID of the note in the database as a string
+    public static FirebaseFirestore db = FirebaseFirestore.getInstance();
 ```
-- This line declares a property named `documentId` of type `String`.
-- It is initialized to an empty string by default and set by the database system.
-- This property represents the document ID of the note in the database.
-
-**Line 6**:
-```java
-    val timestamp: Long = System.currentTimeMillis() // The timestamp of the note's creation as a long integer
-```
-- This line declares a property named `timestamp` of type `Long`.
-- It is initialized to the current system time in milliseconds.
-- This property represents the timestamp of the note's creation.
-
-**Line 7-16**:
-```java
-    constructor(parcel: Parcel) : this( // Constructor that initializes the Note object from a Parcel object
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!
-    ) {}
-
-    override fun describeContents(): Int { // Describe the contents of the Note object for parceling
-        return 0
-    }
-
-    override fun writeToParcel(parcel: Parcel, p1: Int) { // Write the Note object to a Parcel object
-        parcel.writeString(title)
-        parcel.writeString(description)
-        parcel.writeString(documentId)
-    }
-
-    companion object CREATOR : Parcelable.Creator<Note> { // Companion object that creates a Parcelable.Creator instance for the Note class
-
-        override fun createFromParcel(parcel: Parcel): Note { // Create a new Note object from a Parcel object
-            return Note(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Note?> { // Create a new Note object array of the specified size
-            return arrayOfNulls(size)
-        }
-    }
-```
-- These lines define the `Parcelable` implementation for the `Note` class.
-- This allows the `Note` object to be passed between activities and fragments in Android.
+- This line initializes a static field named `db` of type `FirebaseFirestore`.
+- It uses the `getInstance()` method to get a reference to a singleton instance of the FirebaseFirestore database.
+- This instance can be accessed from anywhere within the application to perform database operations.
