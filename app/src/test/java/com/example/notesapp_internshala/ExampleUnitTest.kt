75dcc882437ee0fc2cfec2a500e5d1644eb368c5
Arsh Kumar <arshkum18@gmail.com>
2024-05-03 09:32:59 +0530

    Add documentation for ExampleUnitTest.kt

--- app/src/test/java/com/example/notesapp_internshala/ExampleUnitTest.kt
+++ app/src/test/java/com/example/notesapp_internshala/ExampleUnitTest.kt
@@ -10,17 +10,27 @@ import org.junit.Assert.*;
 
 /**
  * This class contains unit tests for the Notes App developed using the Internshala platform.
+ * Line 1: Package declaration.
+ * Line 2: Import the JUnit library for testing.
+ * Line 4: Define the ExampleUnitTest class for unit testing.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Line 6: Link to documentation for testing.
  */
 public class ExampleUnitTest {
+    // Line 8: ExampleUnitTest class declaration.
+
     // This method tests the addition operation and verifies if the result is correct.
 
     @Test
     public void addition_isCorrect() {
+        // Line 11: Annotate this method as a test method.
+        // Line 12: addition_isCorrect() method declaration.
+
         // This line asserts that the addition of 2 and 2 equals 4. If the assertion fails, the test will fail.
 
         assertEquals(4, 2 + 2);
+        // Line 14: Assert that the addition of 2 and 2 equals 4.
     }
 }
 ```
