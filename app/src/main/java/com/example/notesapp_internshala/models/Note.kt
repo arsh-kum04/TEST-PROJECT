ae2f4b04485146f0949657ee7c4b8620df25c2d9
Arsh Kumar <arshkum18@gmail.com>
2024-05-03 08:41:35 +0530

    Add documentation for Note.kt

--- app/src/main/java/com/example/notesapp_internshala/models/Note.kt
+++ app/src/main/java/com/example/notesapp_internshala/models/Note.kt
@@ -1,12 +1,8 @@
 **Folder Name:** com.example.notesapp_internshala.models
-**File Name:** Note
+**File Name:** Note.java
+
 **Code:**
 ```java
-package com.example.notesapp_internshala.models;
-
-import android.os.Parcel;
-import android.os.Parcelable;
-
 // Data class that represents an individual note
 data class Note(
     // The ID of the current user as a string
@@ -56,3 +52,84 @@ data class Note(
     }
 }
 ```
+
+**Documentation:**
+
+**Line 1**:
+```java
+data class Note( // Data class that represents an individual note
+```
+- This line declares a data class named `Note`.
+- Data classes are used to represent structured data and provide automatic generation of getters, setters, and other methods.
+
+**Line 2**:
+```java
+    val currentUserId: String = "", // The ID of the current user as a string
+```
+- This line declares a property named `currentUserId` of type `String`.
+- It is initialized to an empty string by default.
+- This property represents the ID of the user who created the note.
+
+**Line 3**:
+```java
+    val title: String = "", // The title of the note as a string
+```
+- This line declares a property named `title` of type `String`.
+- It is initialized to an empty string by default.
+- This property represents the title of the note.
+
+**Line 4**:
+```java
+    val description: String = "", // The description of the note as a string
+```
+- This line declares a property named `description` of type `String`.
+- It is initialized to an empty string by default.
+- This property represents the description of the note.
+
+**Line 5**:
+```java
+    var documentId: String = "", // The document ID of the note in the database as a string
+```
+- This line declares a property named `documentId` of type `String`.
+- It is initialized to an empty string by default and set by the database system.
+- This property represents the document ID of the note in the database.
+
+**Line 6**:
+```java
+    val timestamp: Long = System.currentTimeMillis() // The timestamp of the note's creation as a long integer
+```
+- This line declares a property named `timestamp` of type `Long`.
+- It is initialized to the current system time in milliseconds.
+- This property represents the timestamp of the note's creation.
+
+**Line 7-16**:
+```java
+    constructor(parcel: Parcel) : this( // Constructor that initializes the Note object from a Parcel object
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!
+    ) {}
+
+    override fun describeContents(): Int { // Describe the contents of the Note object for parceling
+        return 0
+    }
+
+    override fun writeToParcel(parcel: Parcel, p1: Int) { // Write the Note object to a Parcel object
+        parcel.writeString(title)
+        parcel.writeString(description)
+        parcel.writeString(documentId)
+    }
+
+    companion object CREATOR : Parcelable.Creator<Note> { // Companion object that creates a Parcelable.Creator instance for the Note class
+
+        override fun createFromParcel(parcel: Parcel): Note { // Create a new Note object from a Parcel object
+            return Note(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Note?> { // Create a new Note object array of the specified size
+            return arrayOfNulls(size)
+        }
+    }
+```
+- These lines define the `Parcelable` implementation for the `Note` class.
+- This allows the `Note` object to be passed between activities and fragments in Android.
